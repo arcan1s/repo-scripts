1722e62fd5b6f8c23e97cf001fe7b4b1f4bee7bb
arcan1s <darkarcanis@mail.ru>
2015-02-20 00:17:57 +0300

    release 1.4.0 add daemon with notifications

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Evgeniy "arcanis" Alexeev <esalexeev@gmail.com>
 
 pkgname=repo-scripts
-pkgver=1.3.1
+pkgver=1.4.0
 pkgrel=1
 pkgdesc="A set of scripts to work with your repository"
 arch=('any')
@@ -9,7 +9,7 @@ url="https://github.com/arcan1s/repo-scripts"
 license=("GPLv3")
 depends=('yaourt')
 source=(https://github.com/arcan1s/repo-scripts/releases/download/V.${pkgver}/${pkgname}-${pkgver}-src.tar.xz)
-md5sums=('b3a73e8445f0d0ed5833d1b62ec09609')
+md5sums=('bc1ed9fa137eddb408a5b7034d181cdf')
 backup=('etc/repo-scripts.conf')
 
 package() {
--- create_archive.sh
+++ create_archive.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 ARCHIVE="repo-scripts"
-[ -d ${ARCHIVE} ] && rm -rf ${ARCHIVE}
+[ -d "${ARCHIVE}" ] && rm -rf "${ARCHIVE}"
 
-mkdir -p ${ARCHIVE}/usr/{bin,lib/systemd/system}
-mkdir -p ${ARCHIVE}/etc
+mkdir -p "${ARCHIVE}/usr/"{bin,lib/systemd/system}
+mkdir -p "${ARCHIVE}/etc"
 
-cp sources/repo-check ${ARCHIVE}/usr/bin/
-cp sources/repo-clear ${ARCHIVE}/usr/bin/
-cp sources/repo-update ${ARCHIVE}/usr/bin/
-cp sources/repo-scripts.conf ${ARCHIVE}/etc/
-cp install.sh ${ARCHIVE}/
-cp {AUTHORS,COPYING} ${ARCHIVE}/
+cp "sources/repo-check" "${ARCHIVE}/usr/bin/"
+cp "sources/repo-clear" "${ARCHIVE}/usr/bin/"
+cp "sources/repo-daemon" "${ARCHIVE}/usr/bin/"
+cp "sources/repo-update" "${ARCHIVE}/usr/bin/"
+cp "sources/repo-scripts.conf" "${ARCHIVE}/etc/"
+cp "sources/repo-daemon.service" "${ARCHIVE}/usr/lib/systemd/system/"
+cp "install.sh" "${ARCHIVE}/"
+cp {AUTHORS,COPYING} "${ARCHIVE}/"
 
 VERSION=$(grep Version sources/repo-check | awk '{printf $5;}')
-tar -cf - ${ARCHIVE} | xz -9 -c - > ${ARCHIVE}-${VERSION}-src.tar.xz
-rm -r ${ARCHIVE}
+tar -cf - "${ARCHIVE}" | xz -9 -c - > "${ARCHIVE}-${VERSION}-src.tar.xz"
+rm -rf "${ARCHIVE}"
 
 # update PKGBUILD
-MD5SUMS=$(md5sum ${ARCHIVE}-${VERSION}-src.tar.xz | awk '{print $1}')
+MD5SUMS="$(md5sum "${ARCHIVE}-${VERSION}-src.tar.xz" | awk '{print $1}')"
 sed -i "s/md5sums=('[0-9a-f]\{32\}')/md5sums=('${MD5SUMS}')/" PKGBUILD
 sed -i "s/pkgver=[0-9.]\{5\}/pkgver=${VERSION}/" PKGBUILD
